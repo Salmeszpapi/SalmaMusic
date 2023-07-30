4c6a16c56c04b555a572bd763d7314a25286fdc3
Salmeszpapi <salma638998@gmail.com>
2023-07-30 22:12:11 +0200

    implement drawing features

--- SalmaMusic/Interfaces/IMediaPlayer.cs
+++ SalmaMusic/Interfaces/IMediaPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SalmaMusic.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SalmaMusic.Interfaces
 {
     interface IMediaPlayer
     {
+        void Play();
+        void Pause();
+        void Stop();
+        void LoadMusic(Music music);
+
     }
 }
